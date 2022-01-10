aaf386c36e260bd1ca06ef2462c0403f63f0731f
Eduardo <dutomazoni@gmail.com>
2022-01-10 12:59:54 -0300

    API configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-
+// na hora de buildar, trocar a variavel do .env pelo link do back no heroku
+// https://animadex-backend.herokuapp.com/
 export const api = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
 });
